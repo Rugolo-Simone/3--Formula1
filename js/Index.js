f977b071f43f63986094895aadbd7ac165b5d977
Rugolo-Simone <simonerugolo07@gmail,com>
2024-08-27 16:09:16 +0200

    Prima grafica

---
+++ js/Index.js
@@ -0,0 +1,161 @@
+var piloti = [
+    {
+        "Nome": "Max Verstappen",
+        "Numero": "1",
+        "Team": "Red Bull Racing"
+    },
+    {
+        "Nome": "Sergio Perez",
+        "Numero": "11",
+        "Team": "Red Bull Racing"
+    },
+    {
+        "Nome": "Charles Leclerc",
+        "Numero": "16",
+        "Team": "Ferrari"
+    },
+    {
+        "Nome": "Carlos Sainz",
+        "Numero": "55",
+        "Team": "Ferrari"
+    },
+    {
+        "Nome": "Lewis Hamilton",
+        "Numero": "44",
+        "Team": "Mercedes"
+    },
+    {
+        "Nome": "George Russell",
+        "Numero": "63",
+        "Team": "Mercedes"
+    },
+    {
+        "Nome": "Fernando Alonso",
+        "Numero": "14",
+        "Team": "Aston Martin"
+    },
+    {
+        "Nome": "Lance Stroll",
+        "Numero": "18",
+        "Team": "Aston Martin"
+    },
+    {
+        "Nome": "Lando Norris",
+        "Numero": "4",
+        "Team": "McLaren"
+    },
+    {
+        "Nome": "Oscar Piastri",
+        "Numero": "81",
+        "Team": "McLaren"
+    },
+    {
+        "Nome": "Esteban Ocon",
+        "Numero": "31",
+        "Team": "Alpine"
+    },
+    {
+        "Nome": "Pierre Gasly",
+        "Numero": "10",
+        "Team": "Alpine"
+    },
+    {
+        "Nome": "Valtteri Bottas",
+        "Numero": "77",
+        "Team": "Alfa Romeo"
+    },
+    {
+        "Nome": "Zhou Guanyu",
+        "Numero": "24",
+        "Team": "Alfa Romeo"
+    },
+    {
+        "Nome": "Kevin Magnussen",
+        "Numero": "20",
+        "Team": "Haas"
+    },
+    {
+        "Nome": "Nico Hulkenberg",
+        "Numero": "27",
+        "Team": "Haas"
+    },
+    {
+        "Nome": "Yuki Tsunoda",
+        "Numero": "22",
+        "Team": "AlphaTauri"
+    },
+    {
+        "Nome": "Daniel Ricciardo",
+        "Numero": "3",
+        "Team": "AlphaTauri"
+    },
+    {
+        "Nome": "Alexander Albon",
+        "Numero": "23",
+        "Team": "Williams"
+    },
+    {
+        "Nome": "Logan Sargeant",
+        "Numero": "2",
+        "Team": "Williams"
+    }
+]
+
+var team = [
+    {
+        "Nome": "Scuderia Ferrari",
+        "Vittorie": 2,
+        "Paese": "Italia"
+    },
+    {
+        "Nome": "Red Bull Racing",
+        "Vittorie": 7,
+        "Paese": "Austria"
+    },
+    {
+        "Nome": "Mercedes",
+        "Vittorie": 3,
+        "Paese": "Germania"
+    },
+    {
+        "Nome": "McLaren",
+        "Vittorie": 3,
+        "Paese": "Gran Bretagna"
+    },
+    {
+        "Nome": "Aston Martin",
+        "Vittorie": 0,
+        "Paese": "Gran Bretagna"
+    },
+    {
+        "Nome": "Alpine",
+        "Vittorie": 0,
+        "Paese": "Francia"
+    },
+    {
+        "Nome": "Alfa Romeo",
+        "Vittorie": 0,
+        "Paese": "Svizzera"
+    },
+    {
+        "Nome": "Haas",
+        "Vittorie": 0,
+        "Paese": "Stati Uniti"
+    },
+    {
+        "Nome": "AlphaTauri",
+        "Vittorie": 0,
+        "Paese": "Italia"
+    },
+    {
+        "Nome": "Williams",
+        "Vittorie": 0,
+        "Paese": "Gran Bretagna"
+    }
+]
+
+window.onload = init
+
+function init(){
+    
+}
